ec8b10666cfb03de5d9e4f1103df0da367f4eb05
Dheeraj Kumar <dheerajrajput7056@gmail.com>
2021-07-08 10:42:21 +0530

    changes

--- routes/auth.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController =require('../controllers/auth');
-router.post('/userSignup',authController.register);
-router.post('/signIn',authController.signin);
-
-module.exports = router;
